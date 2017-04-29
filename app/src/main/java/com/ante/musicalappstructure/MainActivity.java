327b48658868d7d735806fc1581a0f5bebb1f2ef
a19p93dev@gmail.com <AP-93@users.noreply.github.com>
2017-04-29 12:55:50 +0200

    2 more activitys added because Project Specification

--- app/src/main/java/com/ante/musicalappstructure/MainActivity.java
+++ app/src/main/java/com/ante/musicalappstructure/MainActivity.java
@@ -8,8 +8,10 @@ import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.RelativeSizeSpan;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TableLayout;
@@ -19,30 +21,33 @@ import android.widget.TextView;
 import static android.graphics.Color.WHITE;
 
 public class MainActivity extends AppCompatActivity
-        implements  View.OnClickListener {
+        implements View.OnClickListener {
 
-
-    TextView tvsongs, tvauthors, tvalbums, tvplaylist;
-    ImageButton repeat,back,play,pause,foward,shuffle;
-
+    TextView tvsongs, tvauthors, tvalbums, tvplaylist, infotv;
+    ImageButton repeat, back, play, pause, foward, shuffle, search;
+    EditText searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        infotv = (TextView) findViewById(R.id.infotv);   //// TODO: 29/04/2017 delete this after review
 
         tvsongs = (TextView) findViewById(R.id.textViewSongs);
         tvauthors = (TextView) findViewById(R.id.textViewArtist);
         tvalbums = (TextView) findViewById(R.id.textViewAlbum);
         tvplaylist = (TextView) findViewById(R.id.textViewPlaylists);
 
-        repeat = (ImageButton)findViewById(R.id.imageButtonRepeat);
-        back = (ImageButton)findViewById(R.id.imageButtonBack);
-        play = (ImageButton)findViewById(R.id.imageButtonPlay);
-        pause = (ImageButton)findViewById(R.id.imageButtonPause);
-        foward = (ImageButton)findViewById(R.id.imageButtonNext);
-        shuffle = (ImageButton)findViewById(R.id.imageButtonShuffle);
+        searchBar = (EditText) findViewById(R.id.editTextSearchSong);
+
+        repeat = (ImageButton) findViewById(R.id.imageButtonRepeat);
+        back = (ImageButton) findViewById(R.id.imageButtonBack);
+        play = (ImageButton) findViewById(R.id.imageButtonPlay);
+        pause = (ImageButton) findViewById(R.id.imageButtonPause);
+        foward = (ImageButton) findViewById(R.id.imageButtonNext);
+        shuffle = (ImageButton) findViewById(R.id.imageButtonShuffle);
+        search = (ImageButton) findViewById(R.id.imageButtonSearch);
 
         repeat.setOnClickListener(this);
         back.setOnClickListener(this);
@@ -50,15 +55,28 @@ public class MainActivity extends AppCompatActivity
         pause.setOnClickListener(this);
         foward.setOnClickListener(this);
         shuffle.setOnClickListener(this);
+        search.setOnClickListener(this);
+
+        infotv.setOnClickListener(this);//for delete
 
         tvsongs.setOnClickListener(this);
         tvauthors.setOnClickListener(this);
         tvalbums.setOnClickListener(this);
         tvplaylist.setOnClickListener(this);
 
-
         tvsongs.setTextColor(Color.parseColor("#87bf34"));
         initSongs();
+
+        //hides search bar after done is pressed on keyboard
+        searchBar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    searchBar.setVisibility(View.INVISIBLE);
+                }
+                return false;
+            }
+        });
 
     }
 
@@ -78,6 +96,7 @@ public class MainActivity extends AppCompatActivity
                 break;
 
             case R.id.textViewAlbum:
+
                 tvalbums.setTextColor(Color.parseColor("#87bf34"));
                 tvsongs.setTextColor(WHITE);
                 tvauthors.setTextColor(WHITE);
@@ -85,68 +104,98 @@ public class MainActivity extends AppCompatActivity
 
                 initAlbums();
                 break;
+
             case R.id.textViewArtist:
+
                 tvauthors.setTextColor(Color.parseColor("#87bf34"));
                 tvsongs.setTextColor(WHITE);
                 tvalbums.setTextColor(WHITE);
                 tvplaylist.setTextColor(WHITE);
+
                 initArtist();
                 break;
+
             case R.id.textViewPlaylists:
+
                 tvplaylist.setTextColor(Color.parseColor("#87bf34"));
                 tvsongs.setTextColor(WHITE);
                 tvalbums.setTextColor(WHITE);
                 tvauthors.setTextColor(WHITE);
+
                 initPlaylists();
                 break;
+
             case R.id.imageButtonBack:
                 break;
+
             case R.id.imageButtonNext:
                 break;
+
             case R.id.imageButtonPause:
+
                 pause.setVisibility(View.GONE);
                 play.setVisibility(View.VISIBLE);
+
                 break;
+
             case R.id.imageButtonPlay:
+
                 play.setVisibility(View.GONE);
                 pause.setVisibility(View.VISIBLE);
+
                 break;
+
             case R.id.imageButtonRepeat:
                 break;
+
             case R.id.imageButtonShuffle:
+                break;
+
+            case R.id.imageButtonSearch:
+
+                searchBar.setVisibility(View.VISIBLE);
+
+                break;
+
+            case R.id.infotv:                                // for delete
+
+                infotv.setVisibility(View.GONE);
+
                 break;
         }
     }
 
-
+     //create rows for table depending on how much songs we have
     //populate view with song names   // TODO: 28/04/2017 conect each view with song
     public void initSongs() {
+
         TableLayout stk = (TableLayout) findViewById(R.id.table);
-        stk.removeAllViews();
+        stk.removeAllViews();   //deletes all table rows
 
         for (int i = 0; i < 25; i++) {    // TODO: 28/04/2017 use number of song loaded in app ass i max
 
             TableRow tbrow = new TableRow(this);
-            TableLayout.LayoutParams rowParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
-                    TableLayout.LayoutParams.WRAP_CONTENT,
-                    Gravity.CENTER_VERTICAL);
+            TableLayout.LayoutParams rowParams = new TableLayout.LayoutParams(             //defined row parameters
+                    TableLayout.LayoutParams.MATCH_PARENT,     //row widith
+                    TableLayout.LayoutParams.WRAP_CONTENT,     //row height
+                    Gravity.CENTER_VERTICAL);                  //row gravity
 
             rowParams.setMargins(0, 1, 0, 1); //left,top,right,bottom
 
-            tbrow.setLayoutParams(rowParams);
+            tbrow.setLayoutParams(rowParams);       //add created parameters to created row
             tbrow.setBackgroundResource(R.drawable.change_color_when_pressed);
             tbrow.setId(i);
 
-            ImageView img = new ImageView(this);
+            ImageView img = new ImageView(this);    //image for row
             img.setBackgroundResource(R.drawable.ic_music_player);  // default image for song if there is no img  TODO: 28/04/2017 check if song hase image to display it
             img.setAdjustViewBounds(true);
             img.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
 
-            TextView t1v = new TextView(this);
+            TextView t1v = new TextView(this);    //song and author name
             String songName = "song name";
             String authorName = "author name";
-            final SpannableString text = new SpannableString(songName + "\n" + authorName);
+            final SpannableString text = new SpannableString(songName + "\n" + authorName);   //alows us to use different text size for each letter in single textview
             text.setSpan(new RelativeSizeSpan(1.8f), 0, songName.length(),
                     Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             text.setSpan(new RelativeSizeSpan(1), 0, authorName.length(),
@@ -159,7 +208,7 @@ public class MainActivity extends AppCompatActivity
             tbrow.addView(img);
             tbrow.addView(t1v);
 
-            tbrow.setOnClickListener(new View.OnClickListener() {
+            tbrow.setOnClickListener(new View.OnClickListener() {     //adds onclickListener to each table row
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(view.getContext(), PlaySongActivity.class);
@@ -167,15 +216,16 @@ public class MainActivity extends AppCompatActivity
                 }
             });
 
-            stk.addView(tbrow, rowParams);
+            stk.addView(tbrow, rowParams);      //add created row to table
         }
 
     }
 
-    private void initPlaylists() {
-    }
 
-    private void initArtist() {
+    private void initPlaylists() {
+    }    //// TODO: 29/04/2017 create playlists and add option to manage them
+
+    private void initArtist() {          //similar to initSongs
 
         TableLayout stk = (TableLayout) findViewById(R.id.table);
 
@@ -230,6 +280,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void initAlbums() {
-    }
+    }    //// TODO: 29/04/2017 add option to read album name from song if there is such info
 
 }
--- app/src/main/java/com/ante/musicalappstructure/PlaySongActivity.java
+++ app/src/main/java/com/ante/musicalappstructure/PlaySongActivity.java
@@ -1,13 +1,62 @@
 package com.ante.musicalappstructure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class PlaySongActivity extends AppCompatActivity {
+import static com.ante.musicalappstructure.R.id.songName;
+
+public class PlaySongActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvsongName, tvinfo2;
+    ImageButton displayInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_song);
+
+
+        tvinfo2 = (TextView) findViewById(R.id.infotv2);
+        displayInfo = (ImageButton) findViewById(R.id.imageButtonDisplayInfo);
+        tvsongName = (TextView) findViewById(songName);
+
+        displayInfo.setOnClickListener(this);
+        tvinfo2.setOnClickListener(this);
+
+
+        String songName = "song name";
+        String authorName = "author name";
+
+        final SpannableString text = new SpannableString(songName + "\n" + authorName);
+        text.setSpan(new RelativeSizeSpan(1.8f), 0, songName.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        text.setSpan(new RelativeSizeSpan(1), 0, authorName.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        tvsongName.setText(text);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.imageButtonDisplayInfo:
+                Intent intent = new Intent(view.getContext(), songInfo.class);
+                startActivity(intent);
+                break;
+
+            case R.id.infotv2:
+                tvinfo2.setVisibility(View.GONE);
+                break;
+
+        }
     }
 }
